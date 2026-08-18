0b70d2e1ea6baae720db6071f2da7cef03671665
fernandesed <fernandesed30@gmail.com>
2026-08-17 21:50:48 -0300

    Add files via upload

---
+++ exemplo1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main()
+{
+     setlocale(LC_CTYPE, "");
+
+     int rgm = 123456;
+    float media = 4.5678;
+    printf("Dados do aluno\n");
+    printf("RGM: %d\n", rgm);
+    printf("Média: %.2f", media);
+        return 0;
+
+
+}
---
+++ ola_mundo.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+
+    int idade = 18;
+    printf("Você tem %d anos de idade.\n", idade);
+
+       return 0;
+}
